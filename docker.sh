0b7a13e2dcaae6b204d8301a972103a85750edd3
ervincheung <1390838101@qq.com>
2019-10-11 00:09:02 +0800

    fix(docker): docker nginx server

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t ervincheung/axshare .
-docker run -it -p 8080:80 -d --rm --name dockerize-axshare ervincheung/axshare
+docker stop axshare
+docker run -p 9666:80 -d --rm --name axshare ervincheung/axshare
